789dff21e875dfb37930410f519a8a54186749f8
abdulhaq-007 <85011586+abdulhaq-007@users.noreply.github.com>
2022-01-31 14:12:13 -0800

    changing a loytttt

--- survey/urls.py
+++ survey/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
   path('login/', auth_views.LoginView.as_view(template_name='survey/login.html'), name='login'),
   path('profile/', views.ProfileView.as_view(), name='profile'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-  path('home/', views.homeView, name='home'),
+  path('', views.homeView, name='home'),
 ]
